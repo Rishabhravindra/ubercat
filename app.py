79c09dd5e1e22f37568e2d0b0e5e6cd46f912b24
ananyanijhawan <nijhawaa@mail.uc.edu>
2017-04-26 14:09:14 -0400

    updated dorms and coordinates

--- app.py
+++ app.py
@@ -27,32 +27,37 @@ def form():
     return render_template('form_submit.html')
 
 # Define a route for the action of the form, for example '/hello/'
-# We are also defining which type of requests this route is 
+# We are also defining which type of requests this route is
 # accepting: POST requests in this case
 # print(geocode_result[0]["geometry"]["location"]["lat"])
 
 
 #Create python dictionary to store dorm co-ordinates and full names
-dorms  = {"danHall":[39.131810, -84.512075,"Daniels Hall"],
-		  "dabHall":[39.131858, -84.512654,"Dabney Hall"],
-		  "morHall":[39.135247, -84.511780,"Morgens Hall"],
-		  "upa":[39.128215, -84.514920, "University Park Apartments"]}
+dorms  = {"danHall":[39.131810, -84.512075, "Daniels Hall"],
+		  "dabHall":[39.131858, -84.512654, "Dabney Hall"],
+		  "morHall":[39.135247, -84.511780, "Morgens Hall"],
+          "sciHall":[39.134303, -84.512053, "Scioto Hall"],
+		  "upa":[39.128215, -84.514920, "University Park Apartments"],
+          "sidHall":[39.128981, -84.517728, "Siddall Hall"],
+          "calHall":[39.128537, -84.516699, "Calhoun Hall"],
+          "strHall":[39.131342, -84.522076, "Stratford Heights"],
+          }
 @app.route('/hello/', methods=['POST'])
 def hello():
     #get data from the submitted form
     dorm = request.form['dorms']
     startLoc = request.form['startLoc'];
     geocode_result = gmapsClient.geocode(startLoc)
-    
+
     response = uberClient.get_price_estimates(
     	start_latitude= geocode_result[0]["geometry"]["location"]["lat"],
 	    start_longitude=  geocode_result[0]["geometry"]["location"]["lng"],
 	    end_latitude= dorms[dorm][0],
 	    end_longitude= dorms[dorm][1]    )
-    
+
     estimates = response.json.get('prices')
-    
-    
+
+
 
     return render_template('form_action.html',
      startLoc=startLoc,
@@ -61,7 +66,7 @@ def hello():
 
 # Run the app :)
 if __name__ == '__main__':
-  app.run( 
+  app.run(
         host="localhost",
         port=int("5000")
   )
